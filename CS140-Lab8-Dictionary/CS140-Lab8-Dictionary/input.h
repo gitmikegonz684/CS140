9ac153209cf2d674f6b69cb358b1ba3916ae99c6
Michael A. Gonzalez <gzalez.ma@gmail.com>
2015-04-14 21:01:45 -0700

    Lab8 reads txt files now

---
+++ CS140-Lab8-Dictionary/CS140-Lab8-Dictionary/input.h
@@ -0,0 +1,22 @@
+//
+//  input.h
+//  CS140-Lab8-Dictionary
+//
+//  Created by Michael A. Gonzalez on 4/13/15.
+//  Copyright (c) 2015 Michael A. Gonzalez. All rights reserved.
+//
+
+#ifndef __CS140_Lab8_Dictionary__input__
+#define __CS140_Lab8_Dictionary__input__
+
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+class Input {
+    public :
+    Input ();
+    int yes=1;
+};
+
+#endif /* defined(__CS140_Lab8_Dictionary__input__) */
--- CS140-Lab8-Dictionary/CS140-Lab8-Dictionary/main.cpp
+++ CS140-Lab8-Dictionary/CS140-Lab8-Dictionary/main.cpp
@@ -12,6 +12,7 @@
 #include <sstream>
 #include <map>
 #include <locale>
+//#include "input.h"
 using namespace std;
 
 int main(int argc, const char * argv[]) {
@@ -19,6 +20,7 @@ int main(int argc, const char * argv[]) {
     ifstream inputFile;
     string line;
     locale l;
+    //Input no;
     map<string, string> words;
     inputFile.open("list.txt");
     
@@ -26,13 +28,25 @@ int main(int argc, const char * argv[]) {
         cout<<"Failed to access file. Goodbye.\n";
         exit(1);
     }
-    inputFile.get();
+    size_t n;
     while (!inputFile.eof()) {
-        cout<<line;
-        inputFile.get();
+        getline(inputFile, line);
+        n = line.find("a");
+        line.replace(n, 1, "$");
+//        for (int i=0; i<line.length(); i++) {
+//            line = line + toupper(line[i], l);
+//        }
+        //in case of "/" at end of lines use code below
+        //line = line
+        //line.erase(line.end());
+        words[line]=line;
+    }
+    for (map <string, string> :: iterator it = words.begin(); it!=words.end(); it++){
+        cout<<it->second<<endl;
     }
     
     //cout<<words["SOLUTION"];
+    cout<<"\ndone";
     
     return 0;
 }
